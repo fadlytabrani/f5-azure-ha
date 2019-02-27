842bca7b144e2f1aba80d166eeddf88811ca3110
Fadly Tabrani <fadly.tabrani@gmail.com>
2019-02-28 02:52:14 +1000

    Update variables.tf

--- terraform-templates/lb-vs/variables.tf
+++ terraform-templates/lb-vs/variables.tf
@@ -1,3 +1,34 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ENVIRONMENT VARIABLES
+# Define secrets as environment variables.
+# ---------------------------------------------------------------------------------------------------------------------
+#
+# Azure subscription details.
+# TF_VAR_AZ_SUBSCRIPTION_ID
+# TF_VAR_AZ_TENANT_ID
+# 
+# F5 BIG-IP virtual machine credentials.
+# TF_VAR_F5_USERNAME
+# TF_VAR_F5_PASSWORD
+#
+#
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# ---------------------------------------------------------------------------------------------------------------------
+variable "AZ_SUBSCRIPTION_ID" {}
+variable "AZ_TENANT_ID" {}
+variable "F5_USERNAME" {}
+variable "F5_PASSWORD" {}
+
 variable "objectname_prefix" {
   default = "f5ha"
+}
+
+variable "vm_size" {
+  default = "Standard_F8s_v2"
+}
+
+variable "f5_version" {
+  default = "14.1.001000"
 }
