e66dfa20ff99fcf2c50f621183efc4d1fae9a19b
Fadly Tabrani <fadly.tabrani@gmail.com>
2019-03-10 13:08:35 +1000

    Update variables.tf

--- terraform-templates/routed-vs/1-nic/variables.tf
+++ terraform-templates/routed-vs/1-nic/variables.tf
@@ -1,35 +1,16 @@
-# ---------------------------------------------------------------------------------------------------------------------
-# ENVIRONMENT VARIABLES
-# Define secrets as environment variables.
-# ---------------------------------------------------------------------------------------------------------------------
-#
-# Azure subscription details.
-# TF_VAR_AZ_SUBSCRIPTION_ID
-# TF_VAR_AZ_TENANT_ID
-# 
-# F5 BIG-IP virtual machine credentials.
-# TF_VAR_F5_USERNAME
-# TF_VAR_F5_PASSWORD
-#
-#
-# ---------------------------------------------------------------------------------------------------------------------
-# REQUIRED PARAMETERS
-# You must provide a value for each of these parameters.
-# ---------------------------------------------------------------------------------------------------------------------
-variable "AZ_SUBSCRIPTION_ID" {}
-variable "AZ_TENANT_ID" {}
-variable "AZ_REGION" {
-  default = "Australia East"
+variable azure_subscription_id {}
+variable azure_tenant_id {}
+variable azure_region {}
+
+variable "f5_username" {}
+variable "f5_license_keys" {
+  type = "list"
 }
-variable "F5_USERNAME" {
-  default = "f5admin"
-}
-variable "F5_PASSWORD" {
-  default = "changethis1please!"
-}
-variable "objectname_prefix" {
+variable "f5_password" {}
+variable "name_prefix" {
   default = "f5ha"
 }
+
 variable "vm_size" {
   default = "Standard_F8s_v2"
 }
@@ -44,13 +25,11 @@ variable "interfaces" {
     "external"
   ]
 }
-
 variable "interface_subnets" {
   default = [
     "10.4.0.0/23"
   ]
 }
-
 variable "routed_subnets" {
    default = [
     "10.4.10.0/24"
